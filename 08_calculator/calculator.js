19f5c51e58178e4a5c9d0e8dcee3f066980b253d
Emre Ilhan <emrilhan@gmail.com>
2023-05-22 17:33:26 +0300

    8th exercise half finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (...args) {
+  let total = 0;
+  args.forEach((element) => {
+    total += element;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+  let total = 1;
+  args.forEach((element) => {
+    total = total * element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  if (num === 1 && num === 0) {
+    return total;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
