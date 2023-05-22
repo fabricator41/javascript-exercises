c64e020571dc6d50a84be19066a374600a524f50
Emre Ilhan <emrilhan@gmail.com>
2023-05-22 15:35:12 +0300

    5th exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+  let sum = 0;
+  if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+    return 'ERROR';
+  } else if (firstNumber < 0 || secondNumber < 0) {
+    return 'ERROR';
+  }
+  const deneme = [firstNumber, secondNumber];
+  deneme.sort((a, b) => a - b);
 
+  for (let i = deneme[0]; i <= deneme[1]; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
