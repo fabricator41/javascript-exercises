503b194f3df3f4bc780a83386f5090621343d9be
Emre Ilhan <emrilhan@gmail.com>
2023-05-31 14:14:34 +0300

    11 exercise complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const deneme = [];
+  books.forEach((element) => {
+    if (element.hasOwnProperty('title')) {
+      deneme.push(element['title']);
+    }
+  });
+  return deneme;
 };
 
 // Do not edit below this line
