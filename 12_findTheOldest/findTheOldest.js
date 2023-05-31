6eb1da288b7881bf83c3513e48be2a32c2efd010
Emre Ilhan <emrilhan@gmail.com>
2023-05-31 14:56:56 +0300

    12 exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, current) => {
+    const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = findAge(current.yearOfBirth, current.yearOfDeath);
 
+    return oldestAge < currentAge ? current : oldest;
+  });
 };
+
+function findAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
