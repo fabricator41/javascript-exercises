d96d563631d1561563b60350c95ba05bd64b49f9
Emre Ilhan <emrilhan@gmail.com>
2023-05-31 14:05:10 +0300

    09 exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const newStr = str.toLowerCase().replace(/[^a-z]/g, '');
+  return newStr === newStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
